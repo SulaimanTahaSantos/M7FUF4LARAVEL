a7ff8f3fc26ed19526d7dfdc137a17cfd98aa7f5
Sulaiman <seltaha@sistemasmedioambientales.com>
2025-05-22 14:41:22 +0000

    agregado usercontroller

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
+use App\Http\Controllers\UserController;
 
 
 // Route::get('/user', function (Request $request) {
