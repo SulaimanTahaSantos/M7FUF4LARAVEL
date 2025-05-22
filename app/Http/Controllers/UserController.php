cc0ad7e1c8b79b5453e53b0e23a5f2c4fabd60ae
Sulaiman <seltaha@sistemasmedioambientales.com>
2025-05-22 14:57:27 +0000

    gestion de usuarios con middleware

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,4 +15,49 @@ class UserController extends Controller
             return response()->json(['message' => 'No users found'], 404);
         }
     }
+
+    public function show($id){
+        $user = User::find($id);
+        if($user){
+            return response()->json($user);
+        } else {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+    }
+
+        public function store(Request $request){
+        $user = new User();
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+        $user->password = Hash::make($request->input('password'));
+        $user->save();
+        return response()->json($user, 201);
+    }
+
+     public function update(Request $request, $id){
+        $user = User::find($id);
+        if ($user) {
+            $user->name = $request->input('name');
+            $user->email = $request->input('email');
+            $user->password = Hash::make($request->input('password'));
+            $user->save();
+            return response()->json($user);
+        } else {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+    }
+    public function destroy($id){
+        $user = User::find($id);
+        if ($user) {
+            $user->delete();
+            return response()->json(['message' => 'User deleted successfully']);
+        } else {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+    }
+
+
+
+
+
 }
--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,19 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\IsUserAdmin;
+use App\Http\Middleware\IsAuthenticated;
 
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
-Route::get('/users', [UserController::class, 'index']);
 
+
+Route::middleware([IsUserAdmin::class])->group(function(){
+    Route::get('/users', [UserController::class, 'index']);
+    Route::get('/users/{id}', [UserController::class, 'show']);
+    Route::put('/users/{id}', [UserController::class, 'update']);
+    Route::delete('/users/{id}', [UserController::class, 'destroy']);
+});
 
