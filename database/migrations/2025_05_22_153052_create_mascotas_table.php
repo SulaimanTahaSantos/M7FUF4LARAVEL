aec3e4e2396a03f23ad905e9213d972968101072
Sulaiman <seltaha@sistemasmedioambientales.com>
2025-05-22 16:09:51 +0000

    importacion de librerias para token

--- database/migrations/2025_05_22_153052_create_mascotas_table.php
+++ database/migrations/2025_05_22_153052_create_mascotas_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('url')->nullable();
             $table->string('description')->nullable();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
